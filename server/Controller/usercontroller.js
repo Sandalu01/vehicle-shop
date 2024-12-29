a88e299c00c25caaf6c1bdea129fe6524cf59bb4
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-12-29 12:27:32 +0530

    ok schema

--- server/Controller/usercontroller.js
+++ server/Controller/usercontroller.js
@@ -1,8 +1,24 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/User.Model');
+const User = require('../Model/User.Model');
+
+
+//add functions
+
+// Create a new product
+const createuser =   async (req, res) => {
+    try {
+      
+      //  const Product = await Product.create(req.body);
+
+      const { username, email, password } = req.body;
+
+      const newuser = new User({ username, email,password });
+      const saved = await newuser.save();
 
 
 
-
-module.exports = router;
+      res.status(200).json(saved);
+    
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
--- server/Model/User.Model.js
+++ server/Model/User.Model.js
@@ -14,7 +14,7 @@ const UserSchema = new mongoose.Schema(
         },
         password: {
             type: String,
-            required: [true, "Please enter user name"]
+            required: [true, "Please enter user  password"]
         }
     },
 
