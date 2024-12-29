d0577f89f5cd1a0e1b35da104618af8eae6ee54a
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-12-29 07:47:21 +0530

    add root

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
 
 const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', async (req, res) => {
     res.send(" hello from node api sandalu thushan ");
