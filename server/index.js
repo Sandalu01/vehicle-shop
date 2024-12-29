dc6e57b9d0187eed9f5b3a8c27ad44c9f755866b
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-12-29 06:35:49 +0530

    nodemon

--- server/index.js
+++ server/index.js
@@ -2,5 +2,10 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-
 const app = express();
+
+app.get('/', async (req, res) => {
+    res.send(" hello from node api sandalu thushan ");
+})
+
+console.log("Hello from node");
