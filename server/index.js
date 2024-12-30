fa6f0687b77bf16c8a1d07d80414e04adc89fa23
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-12-30 10:17:49 +0530

    back end  done conatct details

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const connectDB = require('./config/database.js');
 const User = require('./Model/User.Model.js');
-
+const Contact = require('./Model/Contact.Model.js');
 const app = express();
 
 connectDB();
@@ -22,7 +22,7 @@ console.log("Hello from node");
 app.post('/api/signup', async (req, res) => {
     try {
       
-      //  const Product = await Product.create(req.body);
+        //  const Product = await Product.create(req.body);
 
       const { username, email, password } = req.body;
 
@@ -35,6 +35,25 @@ app.post('/api/signup', async (req, res) => {
       res.status(500).json({ message: error.message });
     }
   });
+
+
+
+  app.post('/api/contact', async (req, res) => {
+    try {
+      
+
+      const { name, email, subject,message } = req.body;
+
+      const userconatct = new Contact({ name, email,subject,message });
+      const saved = await userconatct.save();
+
+      res.status(200).json(saved);
+    
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
