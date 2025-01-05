211674b2d51b4d83e356ba433e8b0401e1e45be7
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2025-01-05 23:49:22 +0530

    done

--- server/Controller/contactcontroller.js
+++ server/Controller/contactcontroller.js
@@ -1,17 +1,21 @@
-const Contact = require('../Model/Contact.Model.js');
+import Contact from '../Model/Contact.Model.js';
 
+const ContactDetails = async (req, res) => {
+  try {
+    const { name, email, subject, message } = req.body;
 
-
-const ContactDetails =   async (req, res) => {
-    try {
-      
-      const { name, email, subject,message, } = req.body;
-
-      const newuser = new Contact({ name, email,subject,message,});
-      const saved = await newuser.save();
-      res.status(200).json(saved);
     
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  };
+    const newContact = new Contact({ name, email, subject, message });
+
+    const saved = await newContact.save();
+
+
+    res.status(200).json(saved);
+  } catch (error) {
+
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+export default ContactDetails;
