0786a56be0226a241c7e9a26f7cab9554ae2cf92
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2025-01-06 12:50:09 +0530

    done emailconfig

---
+++ server/mailtrap/mailtrap.config.js
@@ -0,0 +1,46 @@
+import {MailtrapClient} from "mailtrap";
+
+
+import dotenv from "dotenv";
+
+dotenv.config()
+
+
+const ENDPOINT= process.env.MAILTRAP_ENDPOINT;
+
+// const ENDPOINT = "https://send.api.mailtrap.io/";
+
+export const mailtrapClient = new MailtrapClient({
+  token: process.env.MAILTRAP_TOKEN,
+//   endpoint: ENDPOINT,
+
+});
+
+
+export const sender = {
+  email: "hello@demomailtrap.com",
+  name: "sandalu",
+};
+
+
+
+// it 
+const recipients = [
+  {
+    email: "sandaluthushan20@gmail.com",
+  }
+];
+
+
+
+
+
+// Client
+//   .send({
+//     from: sender,
+//     to: recipients,
+//     subject: "You are awesome!",
+//     html: "Congrats for sending test email with Mailtrap!",
+//     category: "Integration Test",
+//   })
+//   .then(console.log, console.error);
