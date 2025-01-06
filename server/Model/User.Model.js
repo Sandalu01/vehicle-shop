aab8a36ba1f6da0c9c9e2e715416f0c0d017fc27
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2025-01-06 16:26:07 +0530

    user model update

--- server/Model/User.Model.js
+++ server/Model/User.Model.js
@@ -1,45 +1,35 @@
-import mongoose from "mongoose";
-import bcrypt from "bcrypt";
 
-const UserSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: [true, "Please enter user name"],
-    },
-    email: {
-      type: String,
-      required: true,
-      default: 0,
-    },
-    password: {
-      type: String,
-      required: [true, "Please enter user password"],
-    },
-  },
-  {
-    timestamps: true, // Automatically add `createdAt` and `updatedAt` fields
-  }
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+	{
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		lastLogin: {
+			type: Date,
+			default: Date.now,
+		},
+		isVerified: {
+			type: Boolean,
+			default: false,
+		},
+		resetPasswordToken: String,
+		resetPasswordExpiresAt: Date,
+		verificationToken: String,
+		verificationTokenExpiresAt: Date,
+	},
+	{ timestamps: true }
 );
 
-// Hash the password before saving
-UserSchema.pre("save", async function (next) {
-  const user = this;
-
-  // If the password is not modified, skip hashing
-  if (!user.isModified("password")) return next();
-
-  try {
-    // Generate a salt and hash the password
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
-
-const User = mongoose.model("User", UserSchema);
-
-// Export the model as the default export
-export default User;
+export const User = mongoose.model("User", userSchema);
