541b2a833bdd13c0d6c586e7bcfe1e6406d06c03
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2025-01-07 01:35:07 +0530

    add midddlware

--- server/routers/auth.route.js
+++ server/routers/auth.route.js
@@ -3,18 +3,18 @@ const router = express.Router();
 
 import {
     signup,
-    login,
+
     verifyEmail,
  
 } from "../Controller/auth.controller.js";
-import { verifyToken } from "../Middleware/verifyToken.js";
+import { verifyToken } from "../Middleware/verfifyToken.js";
 
 
 
 router.get("/check-auth", verifyToken, checkAuth);
 
-
 router.post("/signup",signup);
+
 router.post("/verify-email", verifyEmail);
 
 export default router;
