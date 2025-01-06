01e7a368feb5937ff032dea9b7d684a0178b203c
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2025-01-07 00:54:04 +0530

    email verify setup

--- server/Controller/auth.controller.js
+++ server/Controller/auth.controller.js
@@ -7,6 +7,7 @@ import { User } from "../Model/User.Model.js";
 import {
 
 	sendVerificationEmail,
+	sendWelcomeEmail,
 } 
 from "../mailtrap/emails.js";
 
@@ -59,3 +60,37 @@ export const signup = async (req, res) => {
 	}
 };
 
+
+//email verify code 
+export const verifyEmail = async (req, res) => {
+	const { code } = req.body;
+	try {
+		const user = await User.findOne({
+			verificationToken: code,
+			verificationTokenExpiresAt: { $gt: Date.now() },
+		});
+
+		if (!user) {
+			return res.status(400).json({ success: false, message: "Invalid or expired verification code" });
+		}
+
+		user.isVerified = true;
+		user.verificationToken = undefined;
+		user.verificationTokenExpiresAt = undefined;
+		await user.save();
+
+		await sendWelcomeEmail(user.email, user.name);
+
+		res.status(200).json({
+			success: true,
+			message: "Email verified successfully",
+			user: {
+				...user._doc,
+				password: undefined,
+			},
+		});
+	} catch (error) {
+		console.log("error in verifyEmail ", error);
+		res.status(500).json({ success: false, message: "Server error" });
+	}
+};
