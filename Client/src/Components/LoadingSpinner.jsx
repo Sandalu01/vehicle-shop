23dce1f8f450d6f985a751ff426087ebf3c91cde
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2025-01-10 14:42:09 +0530

    save loading

--- Client/src/Components/LoadingSpinner.jsx
+++ Client/src/Components/LoadingSpinner.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 
 const LoadingSpinner = () => {
+
 	return (
 		<div className='min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'>
 			{/* Simple Loading Spinner */}
@@ -12,5 +13,4 @@ const LoadingSpinner = () => {
 		</div>
 	);
 };
-
 export default LoadingSpinner;
